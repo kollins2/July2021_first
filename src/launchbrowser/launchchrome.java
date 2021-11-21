3a2887438627ba08d0dc3ccc6af9265b5b705f6f
kollins2 <93236417+kollins2@users.noreply.github.com>
2021-11-20 18:50:17 -0600

    first commit

---
+++ src/launchbrowser/launchchrome.java
@@ -0,0 +1,20 @@
+package launchbrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class launchchrome {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\16462\\July2021_selenium\\1st selenium class\\driver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+
+	}
+	
+	
+}
